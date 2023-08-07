5ce00ee682bc5b663f8e00ff2da757021081c314
Mehdy Haghy <mehdyhaghy@users.noreply.github.com>
2023-08-07 00:05:06 -0500

    Update taxchat1.py

--- taxchat1.py
+++ taxchat1.py
@@ -1,15 +1,7 @@
 import openai
 import streamlit as st
-from langchain.chains import ConversationChain
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    AIMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from langchain.schema import AIMessage, HumanMessage, SystemMessage
 from langchain.chains import LLMChain
 
 
