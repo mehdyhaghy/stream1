df693eb03a89370c80bf66bb32d42ae7adfb3ac7
Mehdy Haghy <mehdyhaghy@users.noreply.github.com>
2023-08-06 20:41:28 -0500

    Update taxchat1.py

--- taxchat1.py
+++ taxchat1.py
@@ -4,11 +4,12 @@ import streamlit as st
 st.title("CPA Chat")
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
-prompt="""As a Tax Resolution Expert ChatBot in the United States provide 
+prompt1="""As a Tax Resolution Expert ChatBot in the United States provide 
 I. Introduction A. What is a Tax Resolution Expert B. Description of Services Offered
 II. The Tax Resolution Process A. Step 1: Determine State of Residence, Marital Status, and Amount Owed to IRS and State B. Step 2: Learn About Relevant Information and Forms for Tax Resolution C. Step 3: Calculate Monthly Payment Amount
 III. Seeking Further Assistance A. Tips for Successfully Resolving Tax Debt B. Contact Information for Professional Tax Resolution Services
 IV. Conclusion A. Recap of Services Offered B. Call to Action for Those Needing Further Assistance: suggest calling (312)-914-7211 """
+st.session_state.messages.append({"role": "assistant", "content": prompt1})
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
@@ -28,7 +29,7 @@ for msg in st.session_state.messages:
 
 if prompt := st.chat_input():
 
-    st.session_state.messages.append({"role": "assistant", "content": prompt})
+    st.session_state.messages.append({"role": "user", "content": prompt})
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     msg = response.choices[0].message
     st.session_state.messages.append(msg)
