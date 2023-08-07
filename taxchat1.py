cf0b483300e9666483b2d782567c4a5b06b34f38
Mehdy Haghy <mehdyhaghy@users.noreply.github.com>
2023-08-06 20:17:14 -0500

    Update taxchat1.py

--- taxchat1.py
+++ taxchat1.py
@@ -1,33 +1,41 @@
+import openai
 import streamlit as st
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
 
-st.title("🦜🔗 Langchain - ChatBot for testing")
+st.title("CPA Chat")
 
-openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-def blog_outline(topic):
-    # Instantiate LLM model
-    llm = OpenAI(model_name="text-davinci-003", openai_api_key=openai_api_key)
-    # Prompt
-    template = """As a Tax Resolution Expert ChatBot in the United States provide provide guidance to {Name}
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+prompt1="""As a Tax Resolution Expert ChatBot in the United States provide 
 I. Introduction A. What is a Tax Resolution Expert B. Description of Services Offered
 II. The Tax Resolution Process A. Step 1: Determine State of Residence, Marital Status, and Amount Owed to IRS and State B. Step 2: Learn About Relevant Information and Forms for Tax Resolution C. Step 3: Calculate Monthly Payment Amount
 III. Seeking Further Assistance A. Tips for Successfully Resolving Tax Debt B. Contact Information for Professional Tax Resolution Services
-IV. Conclusion A. Recap of Services Offered B. Call to Action for Those Needing Further Assistance: suggest calling (312)-914-7211"""
-    prompt = PromptTemplate(input_variables=["Name"], template=template)
-    prompt_query = prompt.format(topic=topic)
-    # Run LLM model
-    response = llm(prompt_query)
-    # Print results
-    return st.info(response)
+IV. Conclusion A. Recap of Services Offered B. Call to Action for Those Needing Further Assistance: suggest calling (312)-914-7211 """
+if prompt := prompt1:
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-
-with st.form("myform"):
-    topic_text = st.text_input("Enter prompt:", "")
-    submitted = st.form_submit_button("Submit")
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-    elif submitted:
-        blog_outline(topic_text)
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
