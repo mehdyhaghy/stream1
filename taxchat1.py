c76f5c458b9dbb7ac34ce45486450981a6871d69
Mehdy Haghy <mehdyhaghy@users.noreply.github.com>
2023-08-06 20:30:55 -0500

    Update taxchat1.py

--- taxchat1.py
+++ taxchat1.py
@@ -27,11 +27,7 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
 
-    openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     #st.chat_message("user").write(prompt)
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
